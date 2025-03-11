4294940ba5aa4a23967bfa7f6b166b8f08279473
Bukasker <kamil.buk.01@gmail.com>
2025-03-11 18:58:09 +0100

    Created Character Combat script

---
+++ Assets/Main/Scripts/Combat/CharacterCombat.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterStats))]
+public class CharacterCombat : MonoBehaviour
+{
+    public float attackSpeed = 1f;
+    public float attackCooldown = 0f;
+    public CharacterStats myStats;
+    private void Update()
+    {
+        attackCooldown -= Time.deltaTime;
+    }
+    public virtual void Attack(CharacterStats targesStats)
+    {
+
+    }
+}
