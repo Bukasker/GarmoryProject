aaae830a1b8267cbd548d1d69eb7578d3540dfed
Bukasker <kamil.buk.01@gmail.com>
2025-03-11 19:02:19 +0100

    Enemy HealthBar script

---
+++ Assets/Main/Scripts/Combat/EnemyHealthBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] private Transform enemyTransform;
+    [SerializeField] private CharacterStats characterStats;
+    [SerializeField] private Slider healthSlider; 
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+
+        healthSlider.value = characterStats.MaxHealthPointsStat.GetValue();
+
+        HealthChanged();
+    }
+
+    public void HealthChanged()
+    {
+        healthSlider.value = characterStats.currentHealth;
+    }
+
+}
