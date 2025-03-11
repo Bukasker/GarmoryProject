55f278740c2fe5da33da5d2ce5d765749c6852fc
Bukasker <kamil.buk.01@gmail.com>
2025-03-11 19:25:54 +0100

    Added text to the slider

--- Assets/Main/Scripts/Stats/EnemyStats.cs
+++ Assets/Main/Scripts/Stats/EnemyStats.cs
@@ -1,4 +1,5 @@
-using Unity.VisualScripting;
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,20 +7,23 @@ public class EnemyStats : CharacterStats
 {
     [SerializeField] private Slider slider;
     [SerializeField] private int maxHealthRange = 500;
+    [SerializeField] private TextMeshProUGUI tmp;
     void Awake()
     {
-        MaxHealthPointsStat.baseValue = Random.Range(0, maxHealthRange);
+        MaxHealthPointsStat.baseValue = UnityEngine.Random.Range(0, maxHealthRange);
         currentHealth = MaxHealthPointsStat.GetValue();
 
         slider.maxValue = MaxHealthPointsStat.GetValue();
         slider.minValue = MinHealth;
-        slider.value = MaxHealthPointsStat.GetValue(); ;
+        slider.value = MaxHealthPointsStat.GetValue();
+        tmp.text = Convert.ToString(currentHealth + "/" + MaxHealthPointsStat.GetValue());
     }
 
     public override void TakeDamage(float damage)
     {
         base.TakeDamage(damage);
         slider.value = currentHealth;
+        tmp.text = Convert.ToString(currentHealth+"/"+MaxHealthPointsStat.GetValue());
     }
 
     public override void Die()
